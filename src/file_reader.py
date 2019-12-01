7b5592b08ef24de57e9a5d22b224d9f124ee2179
Raphael Agra <raphael.agra@ccc.ufcg.edu.br>
2019-12-01 18:10:10 -0300

    Finished project

--- src/file_reader.py
+++ src/file_reader.py
@@ -4,9 +4,6 @@ import csv
 from deputado import *
 from vetor import *
 
-from main import *
-
-
 '''
 Leitura e manipulação do arquivo
 '''
@@ -27,29 +24,3 @@ def ler_votacao(path):
 		voting_dict[dep_nome] = Deputado(dep_nome, dep_estado, dep_partido, Vetor(dep_votos))
 
 	return voting_dict
-
-
-#print(ler_votacao("../data/exemplo.csv"))
-
-
-x = Deputado("teste", "PB", "PSOL", Vetor([1, 1, 1, 1]))
-y = Deputado("biu", "PB", "PSDB", Vetor([-1, -1, -1, 1]))
-z = Deputado("irineu", "AM", "PT", Vetor([1, 1, 0, 0]))
-f = Deputado("joao", "AM", "PT", Vetor([1, 1, 1, 1]))
-p = {"teste":x, "biu":y, "irineu": z, "joao": f}
-p["oi"] = []
-#print(amigos_adocicados(p))
-p['oi'].append(2)
-p['oi'].append(3)
-#print(p)
-
-l =  ["irineu", "biu", "teste"]
-#print(encontra_registro_medio(l, p))
-#print(menos_similar("teste", p))
-
-a = Vetor([2,2])
-b = Vetor([9,8])
-#print(b / (3))
-
-
-
